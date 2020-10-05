c1ef96a3c24813ef4794c18d1520534bce81ae35
Thom Shutt <thomshutt@users.noreply.github.com>
2020-10-05 21:37:10 +0100

    Move read functions to their own file

--- decode/key.go
+++ decode/key.go
@@ -57,35 +57,3 @@ func parseKey(smileBytes []byte) ([]byte, interface{}, error) {
 
 	return nil, nil, fmt.Errorf("unexpected key token: %X", nextByte)
 }
-
-func readLongUTF8(smileBytes []byte) ([]byte, interface{}, error) {
-	var length = 0
-	for smileBytes[length] != STRING_END {
-		length++
-	}
-
-	var s = string(smileBytes[1 : length+1])
-
-	return smileBytes[length+1:], s, nil
-}
-
-func readAscii(smileBytes []byte) ([]byte, interface{}, error) {
-	var length = int(smileBytes[0]&0x1F) + 1
-	var s = string(smileBytes[1 : length+1])
-
-	return smileBytes[length+1:], s, nil
-}
-
-func readTinyUTF8(smileBytes []byte) ([]byte, interface{}, error) {
-	var length = int(smileBytes[0]&0x1F) + 2
-	var s = string(smileBytes[1 : length+1])
-
-	return smileBytes[length+1:], s, nil
-}
-
-func readShortUTF8(smileBytes []byte) ([]byte, interface{}, error) {
-	var length = int(smileBytes[0]&0x1F) + 34
-	var s = string(smileBytes[1 : length+1])
-
-	return smileBytes[length+1:], s, nil
-}
