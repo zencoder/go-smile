4ebc905547cdc501f95f075dab65984bbc0d1121
Thom Shutt <thomshutt@users.noreply.github.com>
2020-10-08 23:26:09 +0100

    Most test cases passing

--- domain/header_test.go
+++ domain/header_test.go
@@ -4,27 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	"github.com/zencoder/go-smile/testdata"
 )
-
-func TestDecodeHeader(t *testing.T) {
-	b := testdata.LoadJSONOrgSample1(t)
-
-	header, err := DecodeHeader(b)
-	require.NoError(t, err)
-
-	require.Equal(
-		t,
-		Header{
-			Version:                           0,
-			RawBinaryPresent:                  false,
-			SharedStringValueEncodingEnabled:  true,
-			SharedPropertyNameEncodingEnabled: true,
-			SizeBytes:                         4,
-		},
-		header,
-	)
-}
 
 func TestDecodeHeaderWithoutData(t *testing.T) {
 	_, err := DecodeHeader([]byte{})
