b250258be132a7d6842e586935fb67db5bb39bcd
Thom Shutt <thomshutt@users.noreply.github.com>
2022-02-19 12:51:42 +0000

    Add support for Big Integer decoding

--- decode/read.go
+++ decode/read.go
@@ -3,6 +3,7 @@ package decode
 import (
 	"fmt"
 	"math"
+	"math/big"
 )
 
 func readVariableLengthText(smileBytes []byte) ([]byte, interface{}, error) {
@@ -27,15 +28,16 @@ func readSimpleLiteral(smileBytes []byte) ([]byte, interface{}, error) {
 	case TRUE:
 		return smileBytes[1:], true, nil
 	case INT_32, INT_64:
-		return readVarInt(smileBytes[1:])
+		return readVarInt(smileBytes[1:], true)
 	case FLOAT_32:
 		return readFloat32(smileBytes[1:])
 	case FLOAT_64:
 		return readFloat64(smileBytes[1:])
+	case BIG_INT:
+		return readBigInt(smileBytes[1:])
 	default:
 		return smileBytes[1:], nil, fmt.Errorf("error reading simple literal byte '%X'", literalByte)
 	}
-	// TODO: BigInteger
 	// TODO: BigDecimal
 }
 
@@ -55,7 +57,7 @@ func readFloat64(smileBytes []byte) ([]byte, interface{}, error) {
 	return smileBytes[10:], math.Float64frombits(intBits), nil
 }
 
-func readVarInt(smileBytes []byte) ([]byte, interface{}, error) {
+func readVarInt(smileBytes []byte, doZigZagDecode bool) ([]byte, int, error) {
 	var varInt, i int
 	for i = 0; smileBytes[i]&0x80 == 0; i++ {
 		varInt = varInt << 7
@@ -64,7 +66,45 @@ func readVarInt(smileBytes []byte) ([]byte, interface{}, error) {
 	varInt = varInt << 6
 	varInt |= int(smileBytes[i] & 0x3F)
 
-	return smileBytes[i+1:], zigzagDecode(varInt), nil
+	if doZigZagDecode {
+		varInt = zigzagDecode(varInt)
+	}
+	return smileBytes[i+1:], varInt, nil
+}
+
+// The format of BigInts is unusual - the initial numeric header tells us how many bytes will be in the
+// resulting number, but the bytes themselves are in the form 0x0xxxxxxx, with the leading 0 to be ignored.
+// We then have to concatenate these seven bits together to "fill up" the output byte array.
+//
+// e.g: We get an input of 00000001 01010101
+// Which then gets turned into an output of 00000011 010101... etc.
+//
+// There's a special case on the final byte, where we just read as many bits (from MSB) as we need to fill up the final
+// output byte, then discard the rest.
+func readBigInt(smileBytes []byte) ([]byte, interface{}, error) {
+	smileBytes, arrayLength, err := readVarInt(smileBytes, false)
+	if err != nil {
+		return smileBytes, arrayLength, err
+	}
+	numBytesToRead := int(math.Ceil(float64(arrayLength*8) / 7))
+
+	var binaryString string
+	for i := 0; i < numBytesToRead; i++ {
+		binaryString += fmt.Sprintf("%07b", smileBytes[i])
+
+		if i == numBytesToRead-1 {
+			trailing := len(binaryString) % 8
+			binaryString = binaryString[:len(binaryString)-trailing]
+		}
+	}
+
+	var n = new(big.Int)
+	n, ok := n.SetString(binaryString, 2)
+	if !ok {
+		return nil, nil, fmt.Errorf("SetString: error turning binary string into Big Int.\nBinary String: %s", binaryString)
+	}
+
+	return smileBytes[numBytesToRead:], n, nil
 }
 
 func zigzagDecode(varInt int) int {
